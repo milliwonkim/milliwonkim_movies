4576316290dc6ecd7cd1e159434eef02fe796d74
milliwonkim <rldnjs9347@gmail.com>
2020-02-18 12:26:33 +0900

    Updates

---
+++ precache-manifest.c3d044c2c11cfea9a046a82f4d6a5663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b1e642330b4f855b7609a6c9af418d",
+    "url": "/milliwonkim_movies/index.html"
+  },
+  {
+    "revision": "fba4697cdb432fd72bb9",
+    "url": "/milliwonkim_movies/static/css/main.7b657550.chunk.css"
+  },
+  {
+    "revision": "2d0452adb3247d70f64e",
+    "url": "/milliwonkim_movies/static/js/2.34b1cbd5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/milliwonkim_movies/static/js/2.34b1cbd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fba4697cdb432fd72bb9",
+    "url": "/milliwonkim_movies/static/js/main.b32868f6.chunk.js"
+  },
+  {
+    "revision": "646fca4ca9687b96f18f",
+    "url": "/milliwonkim_movies/static/js/runtime-main.296e205a.js"
+  }
+]);
