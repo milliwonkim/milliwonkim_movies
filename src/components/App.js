51c66f1f565f85cfc73d36906cc4461774cc5950
milliwonkim <rldnjs9347@gmail.com>
2020-02-18 12:24:55 +0900

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -103,7 +103,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <Header text="MILLIWON MOVIES" />
+      <Header text="Milliwonkim MOVIES" />
       <Search search={search} />
       <p className="App-intro">Sharing a few of our favourite movies</p>
       <div className="movies">
